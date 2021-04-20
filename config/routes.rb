7084ddf5a15f15f37113f5ad0323f924d6936168
yossy8888 <keitaro.yoshida@game8.jp>
2021-04-20 09:10:22 +0900

    インポートとエクスポートの追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
 
   root to: 'tasks#index'
   resources :tasks do
-    post :confirm, action: :confirm_new, on: :new 
+    post :confirm, action: :confirm_new, on: :new
+    post :import, on: :collection 
   end
   resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
